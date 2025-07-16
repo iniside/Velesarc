abd054a6cb5275c4fc2f9b9df967141994d49815
Lukasz Baran <xeavien@gmail.com>
2025-07-16 23:29:42 +0200

    Gameplay Debugger

---
+++ ArcGameplayDebugger/Source/ArcGameplayDebugger/ArcItemInstanceDataDebugger.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+struct FArcItemData;
+struct FArcItemInstance;
+
+class FArcItemInstanceDataDebugger
+{
+public:
+	static void DrawGrantedAbilities(const FArcItemData* InItemData, const FArcItemInstance* InInstance);
+	static void DrawAbilityEffectsToApply(const FArcItemData* InItemData, const FArcItemInstance* InInstance);
+};
