9fe0b91961201388a93b2a89300f15905b60cfb3
Lukasz Baran <xeavien@gmail.com>
2025-07-29 12:57:18 +0200

    Remove scaling by delta time, as it was breaking input.

--- ArcX/ArcCore/Source/ArcCore/Camera/ArcInputAxisBinding2DCameraNode.cpp
+++ ArcX/ArcCore/Source/ArcCore/Camera/ArcInputAxisBinding2DCameraNode.cpp
@@ -1,4 +1,25 @@
-﻿#include "ArcInputAxisBinding2DCameraNode.h"
+﻿/**
+* This file is part of Velesarc
+ * Copyright (C) 2025-2025 Lukasz Baran
+ *
+ * Licensed under the European Union Public License (EUPL), Version 1.2 or –
+ * as soon as they will be approved by the European Commission – later versions
+ * of the EUPL (the "License");
+ *
+ * You may not use this work except in compliance with the License.
+ * You may get a copy of the License at:
+ *
+ * https://eupl.eu/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+#include "ArcInputAxisBinding2DCameraNode.h"
 
 #include "EnhancedInputComponent.h"
 #include "Core/CameraEvaluationContext.h"
@@ -26,6 +47,8 @@ private:
 	TCameraParameterReader<FVector2d> MultiplierReader;
 
 	TArray<FEnhancedInputActionValueBinding*> AxisValueBindings;
+
+	double TimeAccumulator = 0;
 };
 
 UE_DEFINE_CAMERA_NODE_EVALUATOR(FArcInputAxisBinding2DCameraNodeEvaluator)
@@ -79,90 +102,104 @@ void FArcInputAxisBinding2DCameraNodeEvaluator::OnRun(const FCameraNodeEvaluatio
 	FVector2d HighestValue(FVector2d::ZeroVector);
 	double HighestSquaredLenth = 0.f;
 
-	for (FEnhancedInputActionValueBinding* AxisValueBinding : AxisValueBindings)
+	const float FixedUpdateRate = 0.016f / 2;
+
+	// Add the current frame's delta time to our accumulator
+	TimeAccumulator += Params.DeltaTime;
+
+	// Process as many fixed time steps as we've accumulated
+	//while (TimeAccumulator >= FixedUpdateRate)
 	{
-		if (!AxisValueBinding)
+		for (FEnhancedInputActionValueBinding* AxisValueBinding : AxisValueBindings)
 		{
-			continue;
-		}
-
-		const FVector2d Value = AxisValueBinding->GetValue().Get<FVector2D>();
-		const double ValueSquaredLength = Value.SquaredLength();
-		if (ValueSquaredLength > HighestSquaredLenth)
-		{
-			HighestValue = Value;
-		}
-	}
-
-	const FVector2d Multiplier = MultiplierReader.Get(OutResult.VariableTable);
-	DeltaInputValue = FVector2d(HighestValue.X * Multiplier.X, HighestValue.Y * Multiplier.Y);
-	
-	
-	const FVector2d Speed = 
-		SlotNode->bIsPreBlended ?
-			OutResult.VariableTable.GetValue<FVector2d>(SlotNode->GetSpeedVariableID()) :
-			SpeedReader.Get(OutResult.VariableTable);
-
-	FVector2d FinalDelta(DeltaInputValue.X * Speed.X * Params.DeltaTime, DeltaInputValue.Y * Speed.Y * Params.DeltaTime);
-
-	AArcCorePlayerController* PC = Cast<AArcCorePlayerController>(Params.EvaluationContext->GetPlayerController());
-	if (PC)
-	{
-		FinalDelta.X += PC->ControlRotationOffset.Yaw;
-		FinalDelta.Y += PC->ControlRotationOffset.Pitch;
-		PC->ControlRotationOffset.Yaw = 0;
-        PC->ControlRotationOffset.Pitch = 0;
-	}
-	if (RevertAxisXReader.Get(OutResult.VariableTable))
-	{
-		FinalDelta.X = -FinalDelta.X;
-	}
-	if (RevertAxisYReader.Get(OutResult.VariableTable))
-	{
-		FinalDelta.Y = -FinalDelta.Y;
-	}
-
-	//if (bIsAccumulated)
-	{
-		InputValue += FinalDelta;
-	}
-	//else
-	{
-		//	InputValue = FinalDelta;
-	}
-
-
-	
-	InputValue.X = SlotNode->NormalizeX.NormalizeValue(InputValue.X);
-	InputValue.Y = SlotNode->NormalizeY.NormalizeValue(InputValue.Y);
-
-	InputValue.X = SlotNode->ClampX.ClampValue(InputValue.X);
-	InputValue.Y = SlotNode->ClampY.ClampValue(InputValue.Y);
-	if (PC)
-	{
-		if (PC->ClampValue.IsSet())
-		{
-			if (PC->ClampValue.GetValue() < 0)
+			if (!AxisValueBinding)
 			{
-				if (InputValue.Y < PC->ClampValue.GetValue())
-				{
-					InputValue.Y = PC->ClampValue.GetValue();
-				}
+				continue;
 			}
-			else if (PC->ClampValue.GetValue() > 0)
+
+			const FVector2d Value = AxisValueBinding->GetValue().Get<FVector2D>();
+			const double ValueSquaredLength = Value.SquaredLength();
+			if (ValueSquaredLength > HighestSquaredLenth)
 			{
-				if (InputValue.Y > PC->ClampValue.GetValue())
-				{
-					InputValue.Y = PC->ClampValue.GetValue();
-				}
+				HighestValue = Value;
 			}
 		}
-		
-		PC->InputValue = InputValue;
-		
-	}
-	OutResult.VariableTable.SetValue<FVector2d>(SlotNode->GetVariableID(), InputValue);
 
+		const FVector2d Multiplier = MultiplierReader.Get(OutResult.VariableTable);
+		DeltaInputValue = FVector2d(HighestValue.X * Multiplier.X, HighestValue.Y * Multiplier.Y);
+	
+	
+		const FVector2d Speed = 
+			SlotNode->bIsPreBlended ?
+				OutResult.VariableTable.GetValue<FVector2d>(SlotNode->GetSpeedVariableID()) :
+				SpeedReader.Get(OutResult.VariableTable);
+
+		//FVector2d FinalDelta(DeltaInputValue.X * Speed.X * Params.DeltaTime, DeltaInputValue.Y * Speed.Y * Params.DeltaTime);
+		
+		FVector2d FinalDelta(DeltaInputValue.X * Speed.X, DeltaInputValue.Y * Speed.Y);
+		//FVector2d FinalDelta(DeltaInputValue.X * Speed.X * FixedUpdateRate, DeltaInputValue.Y * Speed.Y * FixedUpdateRate);
+		
+		AArcCorePlayerController* PC = Cast<AArcCorePlayerController>(Params.EvaluationContext->GetPlayerController());
+		if (PC)
+		{
+			FinalDelta.X += PC->ControlRotationOffset.Yaw;
+			FinalDelta.Y += PC->ControlRotationOffset.Pitch;
+			PC->ControlRotationOffset.Yaw = 0;
+			PC->ControlRotationOffset.Pitch = 0;
+		}
+		if (RevertAxisXReader.Get(OutResult.VariableTable))
+		{
+			FinalDelta.X = -FinalDelta.X;
+		}
+		if (RevertAxisYReader.Get(OutResult.VariableTable))
+		{
+			FinalDelta.Y = -FinalDelta.Y;
+		}
+
+		//if (bIsAccumulated)
+		{
+			InputValue += FinalDelta;
+		}
+		//else
+		{
+			//	InputValue = FinalDelta;
+		}
+
+
+	
+		InputValue.X = SlotNode->NormalizeX.NormalizeValue(InputValue.X);
+		InputValue.Y = SlotNode->NormalizeY.NormalizeValue(InputValue.Y);
+
+		InputValue.X = SlotNode->ClampX.ClampValue(InputValue.X);
+		InputValue.Y = SlotNode->ClampY.ClampValue(InputValue.Y);
+		if (PC)
+		{
+			if (PC->ClampValue.IsSet())
+			{
+				if (PC->ClampValue.GetValue() < 0)
+				{
+					if (InputValue.Y < PC->ClampValue.GetValue())
+					{
+						InputValue.Y = PC->ClampValue.GetValue();
+					}
+				}
+				else if (PC->ClampValue.GetValue() > 0)
+				{
+					if (InputValue.Y > PC->ClampValue.GetValue())
+					{
+						InputValue.Y = PC->ClampValue.GetValue();
+					}
+				}
+			}
+		
+			PC->InputValue = InputValue;
+		
+		}
+		OutResult.VariableTable.SetValue<FVector2d>(SlotNode->GetVariableID(), InputValue);
+		
+					TimeAccumulator -= FixedUpdateRate;
+        			TimeAccumulator += 0.001;
+	}
 	//Super::OnRun(Params, OutResult);
 }
 }  // namespace UE::Cameras
