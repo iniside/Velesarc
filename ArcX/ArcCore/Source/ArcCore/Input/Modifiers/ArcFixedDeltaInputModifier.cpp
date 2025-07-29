6036936afef02b24d7aab66bd5150a4ff64707ed
Lukasz Baran <xeavien@gmail.com>
2025-07-29 12:56:39 +0200

    Add input modifier, which tries to make input scaling frame indepedent.
    It fill uses defined fixed update rate, and tries to scale incoming input, to match it. Result is more stable than engine provided Scale By Delta. which simply make input slower, but not more consistent.
    
    This modifier, makes input at lower and higher frame rate more consistent. Default to 60fps, should be tweaked to the desired frame target.

---
+++ ArcX/ArcCore/Source/ArcCore/Input/Modifiers/ArcFixedDeltaInputModifier.cpp
@@ -0,0 +1,46 @@
+﻿/**
+* This file is part of Velesarc
+ * Copyright (C) 2025-2025 Lukasz Baran
+ *
+ * Licensed under the European Union Public License (EUPL), Version 1.2 or –
+ * as soon as they will be approved by the European Commission – later versions
+ * of the EUPL (the "License");
+ *
+ * You may not use this work except in compliance with the License.
+ * You may get a copy of the License at:
+ *
+ * https://eupl.eu/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+#include "ArcFixedDeltaInputModifier.h"
+
+FInputActionValue UArcFixedDeltaInputModifier::ModifyRaw_Implementation(const UEnhancedPlayerInput* PlayerInput, FInputActionValue CurrentValue
+	, float DeltaTime)
+{
+	if (CurrentValue.GetValueType() == EInputActionValueType::Boolean)
+	{
+		return CurrentValue; // Only process FVector2D values
+	}
+	
+	const float BaselineDelta = 1.f / DesiredFramerate;
+	
+	if (DeltaTime > 0.0f && DeltaTime != BaselineDelta)
+	{
+		float Scale = BaselineDelta / DeltaTime;
+            
+		// Clamp the scale to prevent extreme values
+		Scale = FMath::Clamp(Scale, 0.1f, 10.0f);
+            
+		FVector2D Input = CurrentValue.Get<FVector2D>();
+		return FInputActionValue(Input * Scale);
+	}
+        
+	return CurrentValue;
+}
---
+++ ArcX/ArcCore/Source/ArcCore/Input/Modifiers/ArcFixedDeltaInputModifier.h
@@ -0,0 +1,49 @@
+﻿/**
+* This file is part of Velesarc
+ * Copyright (C) 2025-2025 Lukasz Baran
+ *
+ * Licensed under the European Union Public License (EUPL), Version 1.2 or –
+ * as soon as they will be approved by the European Commission – later versions
+ * of the EUPL (the "License");
+ *
+ * You may not use this work except in compliance with the License.
+ * You may get a copy of the License at:
+ *
+ * https://eupl.eu/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+#pragma once
+
+#include "InputModifiers.h"
+
+#include "ArcFixedDeltaInputModifier.generated.h"
+
+/**
+ *  Input modifier, which will try to modify any axis into fixed delta time. This will make input more consistent across different framerates.
+ *
+ *  Default to 60.
+ *
+ *  This modifier should be added first, and then the user should add different modifiers, which will modify the normalized value.
+ *  Do not add any other scale by delta time modifiers, as they will make the problem of input being dependent on frame rate more pronouced. 
+ */
+UCLASS(NotBlueprintable, MinimalAPI)
+class UArcFixedDeltaInputModifier : public UInputModifier
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category=Settings, Config, meta=(ClampMin=30, ClampMax=120))
+	float DesiredFramerate = 60.f;
+
+protected:
+
+	virtual FInputActionValue ModifyRaw_Implementation(const UEnhancedPlayerInput* PlayerInput, FInputActionValue CurrentValue, float DeltaTime) override;
+};
