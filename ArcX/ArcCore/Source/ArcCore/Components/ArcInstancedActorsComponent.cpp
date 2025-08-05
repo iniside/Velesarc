cdd6e27cef4b0c49a0204fd644a3f9279946f0cd
Lukasz Baran <xeavien@gmail.com>
2025-08-05 22:40:59 +0200

    Add ArcCoreInstancedActorComponent to expose some functions.

---
+++ ArcX/ArcCore/Source/ArcCore/Components/ArcInstancedActorsComponent.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright Lukasz Baran. All Rights Reserved.
+
+#include "ArcInstancedActorsComponent.h"
+
+// Sets default values for this component's properties
+UArcInstancedActorsComponent::UArcInstancedActorsComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+// Called when the game starts
+void UArcInstancedActorsComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+void UArcInstancedActorsComponent::OnServerPreSpawnInitForInstance(FInstancedActorsInstanceHandle InInstanceHandle)
+{
+	Super::OnServerPreSpawnInitForInstance(InInstanceHandle);
+}
+
+void UArcInstancedActorsComponent::InitializeComponentForInstance(FInstancedActorsInstanceHandle InInstanceHandle)
+{
+	Super::InitializeComponentForInstance(InInstanceHandle);
+}
---
+++ ArcX/ArcCore/Source/ArcCore/Components/ArcInstancedActorsComponent.h
@@ -0,0 +1,30 @@
+﻿// Copyright Lukasz Baran. All Rights Reserved.
+
+#pragma once
+#include "InstancedActorsComponent.h"
+
+#include "ArcInstancedActorsComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ARCCORE_API UArcInstancedActorsComponent : public UInstancedActorsComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UArcInstancedActorsComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	virtual void OnServerPreSpawnInitForInstance(FInstancedActorsInstanceHandle InInstanceHandle) override;
+
+	virtual void InitializeComponentForInstance(FInstancedActorsInstanceHandle InInstanceHandle) override;
+
+	const FInstancedActorsInstanceHandle& GetInstanceHandle() const
+	{
+		return InstanceHandle;
+	}
+};
