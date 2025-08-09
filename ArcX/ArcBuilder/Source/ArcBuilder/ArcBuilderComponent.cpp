e59653551fd80446b79a6fc35d0b3715c091e9b4
Lukasz Baran <xeavien@gmail.com>
2025-08-09 21:17:29 +0200

    More prototyping of builder. Add grid support, and use specific targeting for building to find closest object to snap to when enabled.

--- ArcX/ArcBuilder/Source/ArcBuilder/ArcBuilderComponent.cpp
+++ ArcX/ArcBuilder/Source/ArcBuilder/ArcBuilderComponent.cpp
@@ -25,6 +25,11 @@ void UArcBuilderComponent::BeginPlay()
 	
 }
 
+void UArcBuilderComponent::SetPlacementOffsetLocation(const FVector& NewLocation)
+{
+	PlacementOffsetLocation = NewLocation;
+}
+
 void UArcBuilderComponent::BeginPlacement(UArcBuilderData* InBuilderData)
 {
 	if (GetNetMode() == NM_DedicatedServer)
@@ -32,6 +37,20 @@ void UArcBuilderComponent::BeginPlacement(UArcBuilderData* InBuilderData)
 		return;
 	}
 
+	if (InBuilderData == CurrentBuildData.Get())
+	{
+		return;
+	}
+	else
+	{
+		if (TemporaryPlacementActor)
+		{
+			TemporaryPlacementActor->SetActorHiddenInGame(true);
+			TemporaryPlacementActor->Destroy();
+			TemporaryPlacementActor = nullptr;
+		}
+	}
+	
 	APawn* Pawn = nullptr;
 	if (APlayerController* PC = GetOwner<APlayerController>())
 	{
@@ -64,16 +83,16 @@ void UArcBuilderComponent::BeginPlacement(UArcBuilderData* InBuilderData)
 	}
 	
 	TemporaryPlacementActor = GetWorld()->SpawnActor<AActor>(ActorClass, FVector::ZeroVector, FRotator::ZeroRotator, SpawnParams);
-	
+		
 	UTargetingSubsystem* Targeting = UTargetingSubsystem::Get(GetWorld());
 	FArcTargetingSourceContext Context;
 	Context.InstigatorActor = GetOwner();
 	Context.SourceActor = Pawn;
-	Context.SourceObject = this;
+	Context.SourceObject = TemporaryPlacementActor;
 	
 	if (Targeting)
 	{
-		if (PlacementTargetingPreset)
+		if (PlacementTargetingPreset && !TargetingRequestHandle.IsValid())
 		{
 			TargetingRequestHandle = Arcx::MakeTargetRequestHandle(PlacementTargetingPreset, Context);
 
@@ -87,6 +106,38 @@ void UArcBuilderComponent::BeginPlacement(UArcBuilderData* InBuilderData)
 	}
 }
 
+namespace
+{
+	static FORCEINLINE FVector SnapWorld(const FVector& In, int32 GridSize)
+	{
+		if (GridSize <= 0) return In;
+		const float G = static_cast<float>(GridSize);
+		return FVector(
+			FMath::GridSnap(In.X, G),
+			FMath::GridSnap(In.Y, G),
+			In.Z //FMath::GridSnap(In.Z, G)
+		);
+	}
+
+	static FORCEINLINE FVector SnapRelative(const FVector& In, int32 GridSize, const FVector& Origin, const FQuat& Rotation)
+	{
+		if (GridSize <= 0)
+		{
+			return In;
+		}
+		const float G = static_cast<float>(GridSize);
+		const FTransform GridXform(Rotation, Origin, FVector::OneVector);
+		const FVector Local = GridXform.InverseTransformPosition(In);
+		const FVector SnappedLocal(
+			FMath::GridSnap(Local.X, G),
+			FMath::GridSnap(Local.Y, G),
+			Local.Z //FMath::GridSnap(Local.Z, G)
+		);
+		return GridXform.TransformPosition(SnappedLocal);
+	}
+}
+
+
 void UArcBuilderComponent::HandleTargetingCompleted(FTargetingRequestHandle InTargetingRequestHandle)
 {
 	UTargetingSubsystem* Targeting = UTargetingSubsystem::Get(GetWorld());
@@ -99,37 +150,143 @@ void UArcBuilderComponent::HandleTargetingCompleted(FTargetingRequestHandle InTa
 
 	if (TargetingResults.TargetResults.Num() > 0)
 	{
-		FTargetingDefaultResultData& TargetData = TargetingResults.TargetResults[0];
-		if (TargetData.HitResult.GetActor())
+		UStaticMeshComponent* TargetSMC = nullptr;
+		FHitResult HitResult;
+		FHitResult FirstHitResult = TargetingResults.TargetResults[0].HitResult;
+
+		RelativeGridOrigin = FirstHitResult.GetActor() ? FirstHitResult.GetActor()->GetActorLocation() : FirstHitResult.ImpactPoint;
+
+		if (bAlignGridToSurfaceNormal)
 		{
-			FVector FinalLocation = TargetData.HitResult.ImpactPoint + PlacementOffsetLocation;
-			TemporaryPlacementActor->SetActorLocation(FinalLocation);
+			const FVector Normal = FirstHitResult.ImpactNormal.GetSafeNormal();
+			if (!Normal.IsNearlyZero())
+			{
+				RelativeGridRotation = FRotationMatrix::MakeFromZ(Normal).ToQuat();
+			}
+			else
+			{
+				RelativeGridRotation = FQuat::Identity;
+			}
 		}
+		else
+		{
+			RelativeGridRotation = FQuat::Identity;
+		}
+
+		for (const FTargetingDefaultResultData& ResultData : TargetingResults.TargetResults)
+		{
+			if (ResultData.HitResult.GetActor())
+			{
+				TargetSMC = ResultData.HitResult.GetActor()->FindComponentByClass<UStaticMeshComponent>();
+				HitResult = ResultData.HitResult;
+				if (TargetSMC)
+				{
+					break;	
+				}
+			}
+		}
+		
+		if (TargetSMC && !bUsePlacementGrid)
+		{
+			AActor* TargetActor = HitResult.GetActor();
+			
+			UStaticMeshComponent* MySMC = TemporaryPlacementActor->FindComponentByClass<UStaticMeshComponent>();
+			
+			if (TargetSMC && MySMC)
+			{
+				TArray<FName> TargetSockets = TargetSMC->GetAllSocketNames();
+				TArray<FName> MySockets = MySMC->GetAllSocketNames();
+
+				if (TargetSockets.Num() > 0 && MySockets.Num() > 0)
+				{
+					float SqDistance = FLT_MAX;
+
+					FName TargetSocketName;
+				
+					for (const FName& TargetSocket : TargetSockets)
+					{
+						FVector TargetSocketLocation = TargetSMC->GetSocketLocation(TargetSocket);
+													
+						float NewSqDistance = FVector::DistSquared(TargetSocketLocation, FirstHitResult.ImpactPoint);
+
+						if (NewSqDistance < SqDistance)
+						{
+							SqDistance = NewSqDistance;
+							TargetSocketName = TargetSocket;
+						}
+						
+					}
+
+					if (TargetSocketName.IsValid())
+					{
+						FVector TargetSocketLocation = TargetSMC->GetSocketLocation(TargetSocketName);
+						FVector TargetSocketDir = MySMC->GetSocketRotation(TargetSocketName).Vector();
+						
+						FBoxSphereBounds Bounds = MySMC->Bounds;
+						double Half = Bounds.BoxExtent.X * 0.5;
+
+						SqDistance = FLT_MAX;
+						FName MySocketName;
+						
+						for (const FName& MySocket : MySockets)
+						{
+							FVector MySocketLocation = MySMC->GetSocketLocation(MySocket);
+													
+							float NewSqDistance = FVector::DistSquared(TargetSocketLocation, MySocketLocation);
+
+							if (NewSqDistance < SqDistance)
+							{
+								SqDistance = NewSqDistance;
+								MySocketName = MySocket;
+							}
+						}
+						
+						FVector MySocketLocation = MySMC->GetSocketLocation(MySocketName);
+						float Dist = FVector::Dist(TemporaryPlacementActor->GetActorLocation(), MySocketLocation);
+						FVector NewLocation = TargetSocketLocation + (TargetSocketDir * Dist);
+						TemporaryPlacementActor->SetActorLocation(NewLocation);
+						FinalPlacementLocation = NewLocation;
+						return;
+					}
+				}
+			}
+		}
+		
+		FVector FinalLocation = FirstHitResult.ImpactPoint + PlacementOffsetLocation;
+
+		if (bUsePlacementGrid)
+		{
+			if (bUseRelativeGrid)
+			{
+				FinalLocation = SnapRelative(FinalLocation, CurrentGridSize, RelativeGridOrigin, RelativeGridRotation);
+			}
+			else
+			{
+				const double Grid = static_cast<double>(CurrentGridSize);
+				FinalLocation.X = FMath::GridSnap(FinalLocation.X, Grid);
+				FinalLocation.Y = FMath::GridSnap(FinalLocation.Y, Grid);	
+			}
+				
+		}
+		
+		FinalPlacementLocation = FinalLocation;
+		TemporaryPlacementActor->SetActorLocation(FinalLocation);
+		
 	}
 }
 
 void UArcBuilderComponent::EndPlacement()
 {
-	FTargetingDefaultResultsSet& TargetingResults = FTargetingDefaultResultsSet::FindOrAdd(TargetingRequestHandle);
-
-	if (TargetingResults.TargetResults.Num() > 0)
+	UClass* ActorClass = CurrentBuildData.IsValid() ? CurrentBuildData->ActorClass.LoadSynchronous() : nullptr;
+	if (ActorClass)
 	{
-		FTargetingDefaultResultData& TargetData = TargetingResults.TargetResults[0];
-		if (TargetData.HitResult.GetActor())
-		{
-			UClass* ActorClass = CurrentBuildData.IsValid() ? CurrentBuildData->ActorClass.LoadSynchronous() : nullptr;
-			if (!ActorClass)
-			{
-				FActorSpawnParameters SpawnParams;
-				SpawnParams.Owner = GetOwner();
-				SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-				
-				FVector FinalLocation = TargetData.HitResult.ImpactPoint + PlacementOffsetLocation;
-				GetWorld()->SpawnActor<AActor>(ActorClass, FinalLocation, FRotator::ZeroRotator, SpawnParams);
-			}
-		}
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.Owner = GetOwner();
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		
+		GetWorld()->SpawnActor<AActor>(ActorClass, FinalPlacementLocation, FRotator::ZeroRotator, SpawnParams);
 	}
-
+	
 	if (TemporaryPlacementActor)
 	{
 		TemporaryPlacementActor->SetActorHiddenInGame(true);
@@ -141,7 +298,71 @@ void UArcBuilderComponent::EndPlacement()
 	if (Targeting)
 	{
 		Targeting->RemoveAsyncTargetingRequestWithHandle(TargetingRequestHandle);
+		TargetingRequestHandle.Reset();
 	}
 	
+}
+
+UArcTargetingTask_BuildTrace::UArcTargetingTask_BuildTrace(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UArcTargetingTask_BuildTrace::Execute(const FTargetingRequestHandle& TargetingHandle) const
+{
+	Super::Execute(TargetingHandle);
+	
+	SetTaskAsyncState(TargetingHandle, ETargetingTaskAsyncState::Executing);
+	
+	FArcTargetingSourceContext* Ctx = FArcTargetingSourceContext::Find(TargetingHandle);
+	FTargetingDefaultResultsSet& TargetingResults = FTargetingDefaultResultsSet::FindOrAdd(TargetingHandle);
+
+	if (!Ctx->SourceActor)
+	{
+		SetTaskAsyncState(TargetingHandle, ETargetingTaskAsyncState::Completed);
+		return;
+	}
+	
+	FVector EyeLocation;
+	FRotator EyeRotation;
+	Ctx->SourceActor->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+	FVector EndLocation = EyeLocation + EyeRotation.Vector() * 10000.f;
+	
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(Ctx->SourceActor);
+	Params.bTraceComplex = true;
+	if (AActor* Source = Cast<AActor>(Ctx->SourceObject))
+	{
+		Params.AddIgnoredActor(Source);
+	}
+
+	{
+		FHitResult FirstHitResult;
+		Ctx->InstigatorActor->GetWorld()->LineTraceSingleByChannel(FirstHitResult, EyeLocation, EndLocation, ECC_Visibility, Params);
+
+		FTargetingDefaultResultData* ResultData = new(TargetingResults.TargetResults) FTargetingDefaultResultData();
+		ResultData->HitResult = FirstHitResult;
+	}
+	
+	TArray<FHitResult> HitResults;
+
+	FCollisionResponseParams ResponseParams(ECollisionResponse::ECR_Overlap);
+	
+	Ctx->InstigatorActor->GetWorld()->SweepMultiByChannel(HitResults, EyeLocation, EndLocation, FQuat::Identity, ECC_Visibility, FCollisionShape::MakeSphere(SphereRadius), Params, ResponseParams);
+
+	DrawDebugCylinder(Ctx->InstigatorActor->GetWorld(), EyeLocation, EndLocation, SphereRadius, 10.f, FColor::Green, false, -1.f, 0, 0.2f);
+	for (const FHitResult& HitResult : HitResults)
+	{
+		FTargetingDefaultResultData* ResultData = new(TargetingResults.TargetResults) FTargetingDefaultResultData();
+		ResultData->HitResult = HitResult;
+		DrawDebugPoint(Ctx->InstigatorActor->GetWorld(), HitResult.ImpactPoint, 10.f, FColor::Red, false);
+	}
+	
+	//DrawDebugLine(Ctx->InstigatorActor->GetWorld()
+	//, EyeLocation, EndLocation
+	//, FColor::Blue, false, 0.1, 0, 0.2f);
+	
+	SetTaskAsyncState(TargetingHandle, ETargetingTaskAsyncState::Completed);
 }
 
--- ArcX/ArcBuilder/Source/ArcBuilder/ArcBuilderComponent.h
+++ ArcX/ArcBuilder/Source/ArcBuilder/ArcBuilderComponent.h
@@ -5,11 +5,41 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "Items/Fragments/ArcItemFragment.h"
+#include "Tasks/TargetingTask.h"
 #include "Types/TargetingSystemTypes.h"
 #include "ArcBuilderComponent.generated.h"
 
 class UStaticMesh;
 class UTargetingPreset;
+
+USTRUCT()
+struct FArcBuildSocketInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	FGameplayTagContainer SocketTags;
+
+	UPROPERTY(EditAnywhere)
+	TArray<FName> SocketNames;
+};
+
+class UArcBuilderData;
+class UArcBuilderComponent;
+
+USTRUCT()
+struct ARCBUILDER_API FArcBuildRequirement
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool CanStartPlacement(UArcBuilderData* InBuildData, UArcBuilderComponent* InBuilderComponent) const { return true; }
+
+	virtual bool CanPlace(const FTransform& InTransform, UArcBuilderData* InBuildData, UArcBuilderComponent* InBuilderComponent) const { return true; }
+	
+	virtual ~FArcBuildRequirement() = default;
+};
 
 UCLASS()
 class UArcBuilderData : public UDataAsset
@@ -20,6 +50,30 @@ public:
 	// TODO:: Probably just add StaticMesh, SkinnedMesh and ActorClass, and then just implement spawning directly. YOLO.
 	UPROPERTY(EditAnywhere)
 	TSoftClassPtr<AActor> ActorClass;
+
+	UPROPERTY(EditAnywhere)
+	FArcBuildSocketInfo SocketInfo;
+
+	// Will snap, only if target object hass all these tags.
+	UPROPERTY(EditAnywhere)
+	FGameplayTagContainer SnappingRequiredTags;
+
+	UPROPERTY(EditAnywhere)
+	TArray<TInstancedStruct<FArcBuildRequirement>> BuildRequirements;
+	
+	// Only Sockets with the same name will be matched.
+	UPROPERTY(EditAnywhere)
+	bool bMatchSocketsByName = true;
+	
+	UPROPERTY(EditAnywhere)
+	bool bSnapToClosestSocket = false;
+
+	// Default size of grid cell, when we are using grid snapping. 
+	UPROPERTY(EditAnywhere)
+	int32 DefaultGridSize = 100;
+
+	UPROPERTY(EditAnywhere)
+	FVector ApproximateSize = FVector(100.0, 100.0, 100.0);
 };
 
 USTRUCT()
@@ -50,6 +104,19 @@ protected:
 	TObjectPtr<AActor> TemporaryPlacementActor;
 
 	TWeakObjectPtr<UArcBuilderData> CurrentBuildData;
+	
+	bool bUsePlacementGrid = false;
+
+	// If true, we will use grid relative to location of current object or location.
+	bool bUseRelativeGrid = false;
+	bool bAlignGridToSurfaceNormal = true;
+	
+	FVector RelativeGridOrigin = FVector::ZeroVector;
+	FQuat RelativeGridRotation = FQuat::Identity;
+	
+	int32 CurrentGridSize = 100;
+
+	FVector FinalPlacementLocation = FVector::ZeroVector;
 public:
 	// Sets default values for this component's properties
 	UArcBuilderComponent();
@@ -59,9 +126,53 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
+	void SetUsePlacementGrid(bool bNewUsePlacementGrid)
+	{
+		bUsePlacementGrid = bNewUsePlacementGrid;
+	}
+
+	void SetUseRelativeGrid(bool bInUseRelativeGrid)
+	{
+		bUseRelativeGrid = bInUseRelativeGrid;
+	}
+
+	void SetAlignGridToSurfaceNormal(bool bInAlignGridToSurfaceNormal)
+	{
+		bAlignGridToSurfaceNormal = bInAlignGridToSurfaceNormal;
+	}
+	
+	void SetNewGridSize(int32 NewGridSize)
+	{
+		CurrentGridSize = NewGridSize;
+	}
+	int32 GetGridSize() const
+	{
+		return CurrentGridSize;
+	}
+	void SetPlacementOffsetLocation(const FVector& NewLocation);
+	const FVector& GetPlacementOffsetLocation() const
+	{
+		return PlacementOffsetLocation;
+	}
+	
 	void BeginPlacement(UArcBuilderData* InBuilderData);
 
 	void HandleTargetingCompleted(FTargetingRequestHandle InTargetingRequestHandle);
 
 	void EndPlacement();
+};
+
+UCLASS()
+class UArcTargetingTask_BuildTrace : public UTargetingTask
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Target Trace Selection | Collision Data")
+	float SphereRadius = 400.f;
+	
+public:
+	UArcTargetingTask_BuildTrace(const FObjectInitializer& ObjectInitializer);
+
+	/** Evaluation function called by derived classes to process the targeting request */
+	virtual void Execute(const FTargetingRequestHandle& TargetingHandle) const override;
 };
